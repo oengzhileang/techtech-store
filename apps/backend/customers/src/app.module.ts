611c2ea202143ee1bc22132bbb7ad762d5a0c600
oengzhileang <zhileangCC990@gmail.com>
2025-05-01 17:46:55 +0700

    fix customers service

--- apps/backend/customers/src/app.module.ts
+++ apps/backend/customers/src/app.module.ts
@@ -1,11 +1,47 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { CustomersModule } from './customers/customers.module';
+import databaseConfig from './config/configuration';
 
 @Module({
-  imports: [CustomersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.development',
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('database.uri'),
+        ...(await configService.get('database.option')),
+      }),
+      inject: [ConfigService],
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'), // Path to the uploads folder
+      serveRoot: '/uploads', // URL prefix for accessing images
+    }),
+    CustomersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
--- apps/backend/customers/src/main.ts
+++ apps/backend/customers/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', // Allow requests from your frontend origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+    credentials: true, // Allow cookies or authentication headers if needed
+  });
+  const config = new DocumentBuilder()
+    .setTitle('Nest API')
+    .setDescription('The Nest API description')
+    .setVersion('1.0')
+    .addTag('Nest')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
