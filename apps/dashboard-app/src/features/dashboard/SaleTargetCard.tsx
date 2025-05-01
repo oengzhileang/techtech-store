dac7b94c514e23d4f3a75c022eb182457bfafffb
oengzhileang <zhileangCC990@gmail.com>
2025-05-01 16:42:23 +0700

    implement dashboard UI

--- apps/dashboard-app/src/features/dashboard/SaleTargetCard.tsx
+++ apps/dashboard-app/src/features/dashboard/SaleTargetCard.tsx
@@ -19,7 +19,7 @@ const SaleTargetCard: React.FC<SaleTargetCardProps> = ({
       backgroundColor: "#e5e7eb",
     }}
   >
-    <div className="space-y-7">
+    <div className="space-y-6">
       <h1 className="text-xl font-semibold">{title}</h1>
       <div className="flex justify-between">
         <div>
